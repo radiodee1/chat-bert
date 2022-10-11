4c58fb7f62f4c4e133d470e3c89a2e124592e5c9
David Liebman <radiodee1@gmail.com>
2022-10-11 15:58:45 -0400

    initial download and test. 440M bert base

--- src/main.py
+++ src/main.py
@@ -10,11 +10,6 @@ import transformers
 load_dotenv()
 
 try:
-    AIML_DIR=os.environ['AIML_DIR'] 
-except:
-    AIML_DIR=''
-
-try:
     BATCH_SIZE=int(os.environ['BATCH_SIZE']) 
 except:
     BATCH_SIZE = 32
@@ -54,16 +49,48 @@ try:
 except:
     WEIGHT_PATTERN= 1.0
 
-try:
-    AIML_FILE=str(os.environ['AIML_FILE'])
-except:
-    AIML_FILE = ''
 
-try:
-    SRAI_LITERAL=int(os.environ['SRAI_LITERAL'])
-except:
-    SRAI_LITERAL = 1
+class Kernel:
+
+    def __init__(self):
+        self.verbose = True
+        
+        parser = argparse.ArgumentParser(description="Bert Chat", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        parser.add_argument('--raw-pattern', action='store_true', help='output all raw patterns.')
+        parser.add_argument('--count', action='store_true', help='count number of responses.')
+        parser.add_argument('--name', default='calculate', help='name for "count" operation output files.')
+        parser.add_argument('--disable-ok', action='store_true', help='disable "ok" operation for BERT output.')
+        self.args = parser.parse_args()
+
+        name = [ 'bert-base-uncased', 'bert-large-uncased', 'google/bert_uncased_L-8_H-512_A-8' ]
+        index = BERT_MODEL
+        self.tokenizer = BertTokenizer.from_pretrained(name[index])
+        self.model = BertForNextSentencePrediction.from_pretrained(name[index])
+        pass 
+
+    def bert_batch_compare(self, prompt1, prompt2):
+        encoding = self.tokenizer(prompt1, prompt2, return_tensors='pt', padding=True, truncation=True, add_special_tokens=True, max_length=MAX_LENGTH)
+        #target = torch.LongTensor(self.target)
+        target = torch.ones((1,len(prompt1)), dtype=torch.long)
+        if CUDA == 1:
+            encoding = encoding.to('cuda')
+            target = target.to('cuda')
+
+        #outputs = self.model(**encoding, next_sentence_label=target)
+        outputs = self.model(**encoding, labels=target)
+        logits = outputs.logits.detach()
+        #print(outputs, '< logits')
+        return logits
 
 
+if __name__ == '__main__':
 
-print("here")
+    k = Kernel()
+    p1 = ["hi there", "hi there", "hello", "this is making no sense", "go to the moon", "1 2 3 ", "4 5 6 ", "do not go."]
+    p2 = ["hello",   "hi there", "hello" , "go to the moon", "go to the moon", "4 5 6 ", "1 2 3 ", "don't go."]
+    logits = k.bert_batch_compare(p1, p2)
+    if self.verbose: 
+        print(p1)
+        print(p2)
+    print(logits)
+
