26479dd9315e15e5e34c0f8e8ad6a2101823a1e5
David Liebman <radiodee1@gmail.com>
2022-10-16 11:43:03 -0400

    build files for simple operation of main.py

--- src/build_files.py
+++ src/build_files.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3.10
 
 import argparse 
+import os 
+import shutil 
 
 class Writer:
 
@@ -15,10 +17,12 @@ class Writer:
         parser.add_argument('--name', default='./../data/phrases.txt.orig', help='name for "phrases" input file.')
         parser.add_argument('--list', action='store_true', help='list all possible phrases.')
         parser.add_argument('--verbose', action="store_true", help="print verbose output.")
+        parser.add_argument('--write', action="store_true", help="change file contents")
         self.args = parser.parse_args()
         
         self.verbose = self.args.verbose 
         self.list = self.args.list 
+        self.write = self.args.write 
 
     def read_input_file(self):
         with open(self.args.name, "r") as phrases:
@@ -31,9 +35,31 @@ class Writer:
             pass 
         print(self.phrases)
 
+    def write_output_files(self):
+        try: 
+            s = os.stat(self.args.name)
+        except:
+            exit()
+        if self.write: 
+            shutil.copy(self.args.name, "./../data/phrases.txt")
+        for i in range(len(self.phrases) ):
+            line_ending = "_" + ("000" + str(i + 1))[-3:] + ".txt"
+            if self.verbose: 
+                print(line_ending)
+
+            if i != 0 and self.write: 
+                with open("./../data/room" + line_ending, "w") as rooms:
+                    for ii in range(len(self.phrases) ):
+                        rooms.write(self.phrases[ii][2] + ";1.0" + "\n")
+
+            if self.write: 
+                with open("./../data/responses" + line_ending, "w") as responses:
+                    responses.write("1\n" + self.phrases[i][1] + "\n")
+            pass 
+
 
 if __name__ == '__main__':
     w = Writer()
     w.read_input_file()
-
+    w.write_output_files()
 
