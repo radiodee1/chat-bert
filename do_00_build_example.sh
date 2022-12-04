ef62c53ebe4e0a5f56b4c99438ef132b0dd0b3c3
David Liebman <radiodee1@gmail.com>
2022-12-04 11:34:41 -0500

    add mixin room

--- do_00_build_example.sh
+++ do_00_build_example.sh
@@ -4,6 +4,7 @@ cd ./src/
 
 ./build_multipliers.py --write --room 1 --list  
 ./build_multipliers.py --write --room 2 --list 
+./build_multipliers.py --write --room 3 --list 
 ./build_multipliers.py --write --room 5 --list 
 
-echo "example using rooms 1,2, and 5"
+echo "example using rooms 1,2,3 and 5"
